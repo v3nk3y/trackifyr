f6293002311290b3abab47a5d5ee2771d97f89c5
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-19 20:17:36 -0400

    Trkr: Install Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes</Text>
+        <Button>Lets go</Button>
+      </Flex>
+    </div>
+  );
 };
 
 export default IssuesPage;
