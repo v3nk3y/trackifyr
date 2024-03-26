854c6b06856cace652c9d28811829d7f24ac813b
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-26 13:02:07 -0400

    Trkr: Navbar changes to add logout menu and avatar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { Avatar, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
+import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { PiBugDroidFill } from "react-icons/pi";
-import classNames from "classnames";
-import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -13,37 +13,61 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
-  const { status, data } = useSession();
+  const { status, data: session } = useSession();
   return (
-    <nav className="flex space-x-6 border-b-2 mb-2 px-5 h-14 items-center">
-      <Link href="/" className="flex items-center">
-        <PiBugDroidFill size={20} />
-        <span className="text-xl font-medium">Trakifyr</span>
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classNames({
-                "text-gray-900": link.href === currentPath,
-                "text-gray-500": link.href !== currentPath,
-                "hover:text-gray-800 transition-colors font-medium": true,
-              })}
-            >
-              {link.label}
+    <nav className="border-b-2 mb-2 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="5">
+            <Link href="/" className="flex items-center">
+              <PiBugDroidFill size={20} />
+              <span className="text-xl font-medium">Trakifyr</span>
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classNames({
+                      "text-gray-900": link.href === currentPath,
+                      "text-gray-500": link.href !== currentPath,
+                      "hover:text-gray-800 transition-colors font-medium": true,
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex align="center" gap="5">
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };
