46e14139d4bbfeee643b260ee0980d98cc5b8cf9
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-26 16:11:49 -0400

    Trkr: Fetch users for assigne select, fix avatar not loading issue(Refferer-policy)

--- app/issues/[id]/SelectAssignee.tsx
+++ app/issues/[id]/SelectAssignee.tsx
@@ -1,15 +1,30 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const SelectAssignee = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root defaultValue="" size="2">
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="v3nk3y">v3nk3y</Select.Item>
-          <Select.Item value="venkatesh">Venkatesh</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
