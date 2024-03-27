3564ea6da6eedfbd2083ed7106fc028544343138
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-27 12:54:46 -0400

    Trkr:Build Issue Status filter component

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
+
+const IssueStatusFilter = () => {
+  return (
+    <Select.Root defaultValue="apple">
+      <Select.Trigger />
+      <Select.Content>
+        <Select.Group>
+          {statuses.map((status) => (
+            <Select.Item key={status.value} value={status.value || "ALL"}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
